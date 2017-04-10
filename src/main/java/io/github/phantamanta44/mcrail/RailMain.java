d10a5d99cd1ffd5abbe0f9e5f371004adf864b98
phantamanta44 <evandalong@gmail.com>
2017-04-10 15:44:40 -0500

    Initial commit

---
+++ src/main/java/io/github/phantamanta44/mcrail/RailMain.java
@@ -0,0 +1,17 @@
+package io.github.phantamanta44.mcrail;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RailMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Initialize stuff
+    }
+
+    @Override
+    public void onDisable() {
+        // Clean stuff up
+    }
+
+}
