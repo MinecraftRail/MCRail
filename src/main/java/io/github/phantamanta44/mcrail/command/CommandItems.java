ca2398588e8b2b299f80218ab2edf3e6f4513d54
phantamanta44 <evandalong@gmail.com>
2017-04-23 16:05:32 -0500

    More comprehensive /ritems command

--- src/main/java/io/github/phantamanta44/mcrail/command/CommandItems.java
+++ src/main/java/io/github/phantamanta44/mcrail/command/CommandItems.java
@@ -1,13 +1,17 @@
 package io.github.phantamanta44.mcrail.command;
 
 import io.github.phantamanta44.mcrail.Rail;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 import java.util.stream.Collectors;
 
 public class CommandItems implements CommandExecutor {
@@ -18,10 +22,37 @@ public class CommandItems implements CommandExecutor {
             sender.sendMessage(ChatColor.RED + "No permission!");
             return true;
         }
-        sender.sendMessage(new String[] {
-                ChatColor.GRAY + "Valid Item IDs:",
-                Rail.itemRegistry().stream().map(Map.Entry::getKey).collect(Collectors.joining("\n"))
-        });
+        int page = 1;
+        if (args.length > 0) {
+            try {
+                page = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                try {
+                    Pattern pattern = Pattern.compile(StringUtils.join(args), Pattern.CASE_INSENSITIVE);
+                    sender.sendMessage(new String[]{
+                            ChatColor.GRAY + String.format("Item IDs Containing /%s/:", pattern.pattern()),
+                            Rail.itemRegistry().stream()
+                                    .map(Map.Entry::getKey)
+                                    .filter(pattern.asPredicate())
+                                    .sorted()
+                                    .collect(Collectors.joining("\n"))
+                    });
+                } catch (PatternSyntaxException e2) {
+                    sender.sendMessage(ChatColor.RED + "Invalid regular expression!");
+                }
+                return true;
+            }
+        }
+        List<String> ids = Rail.itemRegistry().stream().map(Map.Entry::getKey).sorted().collect(Collectors.toList());
+        int pages = (int)Math.ceil((float)ids.size() / 8F);
+        if (page < 1 || page > pages) {
+            sender.sendMessage(ChatColor.RED + "Invalid page number!");
+        } else {
+            sender.sendMessage(new String[]{
+                    ChatColor.GRAY + String.format("Valid Item IDs (Page %d of %d):", page, pages),
+                    ids.stream().skip((page - 1) * 8).limit(8).collect(Collectors.joining("\n"))
+            });
+        }
         return true;
     }
 
