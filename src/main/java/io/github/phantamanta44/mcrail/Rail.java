89f3b3e4a55b2ba8a6219e581a9e576e6bfffddd
phantamanta44 <evandalong@gmail.com>
2017-04-30 16:42:37 -0500

    Remove recipe test code

--- src/main/java/io/github/phantamanta44/mcrail/Rail.java
+++ src/main/java/io/github/phantamanta44/mcrail/Rail.java
@@ -3,7 +3,9 @@ package io.github.phantamanta44.mcrail;
 import io.github.phantamanta44.mcrail.adapter.AdapterRegistry;
 import io.github.phantamanta44.mcrail.command.CommandItem;
 import io.github.phantamanta44.mcrail.command.CommandItems;
-import io.github.phantamanta44.mcrail.crafting.*;
+import io.github.phantamanta44.mcrail.crafting.CraftingHandler;
+import io.github.phantamanta44.mcrail.crafting.RecipeManager;
+import io.github.phantamanta44.mcrail.crafting.SmeltingHandler;
 import io.github.phantamanta44.mcrail.fluid.FluidBucketAdapter;
 import io.github.phantamanta44.mcrail.fluid.FluidRegistry;
 import io.github.phantamanta44.mcrail.fluid.IFluidContainer;
@@ -15,7 +17,6 @@ import io.github.phantamanta44.mcrail.sign.SignManager;
 import io.github.phantamanta44.mcrail.sign.SignRegistry;
 import io.github.phantamanta44.mcrail.sign.WorldDataHandler;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
@@ -52,14 +53,6 @@ public class Rail extends JavaPlugin {
         signReg = new SignRegistry();
         itemReg = new ItemRegistry();
         recipeMan = new RecipeManager();
-        recipeMan.register(new RailSmeltRecipe()
-                .withInput(Material.ICE)
-                .withOutput(new ItemStack(Material.SNOW_BALL))); // FIXME Remove test code
-        recipeMan.register(new RailShapelessRecipe()
-                .ingredient(Material.IRON_INGOT)
-                .ingredient(Material.REDSTONE)
-                .ingredient(Material.GLOWSTONE)
-                .withOutput(new ItemStack(Material.GOLD_INGOT))); // FIXME Remove test code
         fluidReg = new FluidRegistry();
         blockAdapterReg = new AdapterRegistry<>();
         itemAdapterReg = new AdapterRegistry<>();
